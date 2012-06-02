5db2ff7a90b13d3e7136904ecd16feb9e2d29721
redka <thisredoned@gmail.com>
2012-06-02 20:12:47 +0200

    kropki

--- jtmseditor.rb
+++ jtmseditor.rb
@@ -118,7 +118,7 @@ class Jtmseditor < Processing::App
     @edges.group_by { |x| x.nodes[1] }.map do |node, edges|
       ins = edges.map { |x| x.nodes[0].id }.join ','
       negs = edges.map { |x| x.negated? ? '1' : '0' }.join ','
-      "bramka(#{node.id},[#{ins}],[#{negs}])\n"
+      "bramka(#{node.id},[#{ins}],[#{negs}]).\n"
     end.each { |x| output << x }
     @objects.select { |x| !@edges.find { |e| e.nodes[1] == x } }.each do |node|
       output << "bramka(#{node.id},[],[])\n"
